1cee7e03f75e8e789b5e7a3738c71deaaa949765
aa163 <59177096+aa163@users.noreply.github.com>
2019-12-23 22:50:13 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,46 @@
+class D
+{
+	void dis()
+	{
+		
+		System.out.println("dis()");
+		
+	}
+
+}
+
+
+class C
+{
+
+	int x;
+	static int p=100;;
+
+	void show()
+	{
+		
+		x=10;
+		System.out.println("show()");
+		System.out.println("x="+x);
+		
+		
+	}
+
+
+
+
+
+	public static void main(String ar[])
+	{
+
+		System.out.println("hello");
+
+		C c1=new C();
+		//c1.show();
+		int x[]=new int[2];
+
+		
+		
+	}
+
+}
